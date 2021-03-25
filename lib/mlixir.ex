1d1b28b728f51a7139e8db3e3ed3628aa2c2cf3c
Karol Szuster <karol.szuster@upsidelab.io>
2021-03-25 12:37:33 +0100

    Initialize mix project

---
+++ lib/mlixir.ex
@@ -0,0 +1,18 @@
+defmodule Mlixir do
+  @moduledoc """
+  Documentation for `Mlixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Mlixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mlixir_test.exs
@@ -0,0 +1,8 @@
+defmodule MlixirTest do
+  use ExUnit.Case
+  doctest Mlixir
+
+  test "greets the world" do
+    assert Mlixir.hello() == :world
+  end
+end
