806eb2fd4a00d89b9c158df1d4bfda8adbf4a3cb
Karol Szuster <karol.szuster@upsidelab.io>
2021-03-29 01:29:02 +0200

    Add semi-working SGD implementation

--- lib/mlixir/stochastic_gradient_descent.ex
+++ lib/mlixir/stochastic_gradient_descent.ex
@@ -5,15 +5,40 @@ defmodule Mlixir.StochasticGradientDescent do
 
   @behaviour Mlixir.Model
 
-  alias Mlixir.Shared
-
   import Nx.Defn
+
+  @learning_rate 0.01
+
+  @epochs 1_000
 
   @doc """
   Train the SGD model.
+
+  Uses mean squared error by default.
   """
   @impl true
-  defn fit(x, y) do
+  def fit(%Nx.Tensor{shape: {_, n_coef}} = x, y) do
+    coefficients = Nx.broadcast(0, {n_coef})
+    Enum.reduce(0..@epochs, coefficients, fn _, acc -> gradient_step(acc, x, y) end)
+  end
+
+  defnp gradient_step(coefficients, x, y) do
+    error = grad(coefficients, fn coeff -> loss(coeff, x, y) end)
+    coefficients - (error * @learning_rate)
+  end
+
+  defnp loss(coefficients, x, y) do
+    y_pred = Nx.dot(x, coefficients)
+    mean_squared_error(y_pred, y)
+  end
+
+  defnp mean_squared_error(y_pred, y) do
+    {n_samples} = Nx.shape(y_pred)
+
+    y - y_pred
+    |> Nx.power(2)
+    |> Nx.sum()
+    |> Nx.divide(n_samples)
   end
 
   @doc """
@@ -21,5 +46,6 @@ defmodule Mlixir.StochasticGradientDescent do
   """
   @impl true
   defn predict(model, x) do
+    Nx.dot(model, x)
   end
 end
