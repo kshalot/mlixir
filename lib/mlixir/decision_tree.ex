39d05c9d3865a5d2621f6854612473851335206f
Karol Szuster <karol.szuster@upsidelab.io>
2021-04-24 23:52:42 +0200

    Implement DecisionTree predict method for GEMM traversal

--- lib/mlixir/decision_tree.ex
+++ lib/mlixir/decision_tree.ex
@@ -12,8 +12,8 @@ defmodule Mlixir.DecisionTree do
   @doc """
   Train the decision tree.
 
-  The result of this function are three tensors representing
-  the Decision Tree:
+  The result of this function are four tensors representing
+  the Decision Tree (allowing for GEMM traversal):
 
   F - tensor of shape {attribute_count, node_count} encoding
       the mapping between nodes and input attributes.
@@ -25,7 +25,11 @@ defmodule Mlixir.DecisionTree do
       -1: right
       0: not a child
 
-  C - tensor of shape {class_count} containg the values/classes
+  D - tensor of shape {class_count} encoidng the number of
+      "left" edges encountered during traversing up from the node
+      to the root of the tree
+
+  C - tensor of shape {class_count} containing the values/classes
       in each terminal node.
   """
   @impl true
@@ -54,8 +58,18 @@ defmodule Mlixir.DecisionTree do
 
   @doc """
   Predict using the trained model.
+
+  The result for a given input 'x' is calculated with a simple formula,
+  using the output tensors of the 'fit/2' method:
+
+  ((x @ F @ S) == D) @ C
   """
   @impl true
-  defn predict(model, x) do
+  defn predict({f, s, d, c} = model, x) do
+    x
+    |> Nx.dot(f)
+    |> Nx.dot(s)
+    |> Nx.equal(d)
+    |> Nx.dot(c)
   end
 end
