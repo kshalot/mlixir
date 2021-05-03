07d90a42723319daf4d6752b0ea24b1b8a3bbd6f
kshalot <kszuster@protonmail.com>
2021-05-03 23:18:47 +0200

    Add FIXME to model.ex

--- lib/mlixir/model.ex
+++ lib/mlixir/model.ex
@@ -1,4 +1,8 @@
 defmodule Mlixir.Model do
+  # FIXME Revise this API - it's impossible to use things like
+  # options and still fully adhere to it. Probably best to replace
+  # with sth like a protocol + struct (maybe?)
+
   @moduledoc """
   Behaviour defining the public API of every Mlixir model.
   """
