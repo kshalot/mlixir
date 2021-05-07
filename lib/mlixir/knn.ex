1949b676e6858f22ac8c0dbf94fd4b55edd0a55d
kshalot <kszuster@protonmail.com>
2021-05-07 23:02:24 +0200

    Add not full implementation on kNN

--- lib/mlixir/knn.ex
+++ lib/mlixir/knn.ex
@@ -1,6 +1,6 @@
 defmodule Mlixir.KNN do
   @moduledoc """
-  Model representing a k-nearest neighbours classifier.
+  Model representing a k-nearest neighbours regressor/classifier.
   """
 
   import Nx.Defn
@@ -11,12 +11,23 @@ defmodule Mlixir.KNN do
   """
   @impl true
   defn fit(x, y) do
-    Nx.stack(x, y)
+    {x, y}
   end
 
   @doc """
   """
   @impl true
-  defn predict(model, x) do
+  defn predict({x_model, _}, x) do
+    x
+    |> Nx.subtract(x_model)
+    |> batch_euclidean_norm()
+    |> Nx.argsort()
+  end
+
+  defnp batch_euclidean_norm(t) do
+    t
+    |> Nx.power(2)
+    |> Nx.sum(axes: [1])
+    |> Nx.power(0.5)
   end
 end
